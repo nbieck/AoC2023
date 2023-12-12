7c8084c91e4d9c318c61c59ffbef2d729d119415
Niklas Bieck <niklasjbieck@gmail.com>
2023-12-12 16:38:21 +0900

    Day 1 Part 2

--- 01/d01.py
+++ 01/d01.py
@@ -1,8 +1,20 @@
+import re
+
 if __name__ == "__main__":
+
+    pattern = re.compile(r"(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))")
+    translate = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9,
+                 '0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
+
     with open("input.txt") as input:
         sum = 0
         for l in input:
-            digits = [ord(c) - ord('0') for c in l if c in "0123456789"]
+            print(l)
+            matches = pattern.findall(l)
+            print(matches)
+            digits = [translate[m] for m in matches]
+            print(digits)
+            print('\n')
             val = digits[0] * 10 + digits[-1]
             sum += val
         print(sum)
